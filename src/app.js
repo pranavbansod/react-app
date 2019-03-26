3c8ffa7adb9ac1c7d945b51e3203d93752e09d4c
Pranavb <pranavb@IN-Pranavb.local>
2019-03-26 14:45:54 +0530

    moved character state from app level to table level

--- src/app.js
+++ src/app.js
@@ -3,33 +3,10 @@ import React, {Component} from 'react'
 import Table from './table'
 
 class App extends Component {
-    state = {
-        characters: [{
-            name: 'Patel',
-            job: 'Man U Manager',
-        },
-            {
-                name: 'Mac',
-                job: 'Bouncer',
-            }]
-    };
-
-    removeCharacter = index => {
-        const { characters } = this.state;
-
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index
-            }),
-        })
-
-    };
-
     render() {
-        const {characters} = this.state;
 
         return <div className="App">
-            <Table characters={characters} removeCharacter={this.removeCharacter}/>
+            <Table />
         </div>;
     };
 }
--- src/table.js
+++ src/table.js
@@ -4,8 +4,31 @@ import TableHead from './table/tableHead';
 import TableBody from './table/tableBody';
 
 class Table extends Component {
+    state = {
+        characters: [{
+            name: 'Patel',
+            job: 'Man U Manager',
+        },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            }]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+
+    };
+
+
     render() {
-        const { characters, removeCharacter } = this.props;
+        const { characters, removeCharacter } = this.state;
         return (
             <table>
                 <TableHead/>
